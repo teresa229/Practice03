73fe1d62797404bbb8a83e9469de0c08a547836e
teresa229 <scarlett_29@naver.com>
2020-11-19 10:55:35 +0900

    수정

--- src/com/javaex/practice/Ex16.java
+++ src/com/javaex/practice/Ex16.java
@@ -6,18 +6,21 @@ public class Ex16 {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("숫자를 입력하세요: ");
+		System.out.print("숫자를 입력하세요: ");
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
-		int i;
-		int sum;
+		int sum = 0;
 		
+		for (int i=1; i<=num; i++) {
+			if(num%5 == 0) {
+			sum = sum + i;
+			}	
+		}
 		System.out.println("5의 배수의 개수: "+ num/5);
-		
-		
-		System.out.println("5의 배수의 합" + sum);  //합이 어떻게 되냐?
-				}
-		
+		System.out.println("5의 배수의 합: " + sum);  //합이 어떻게 되냐?
+				
+	
+		sc.close();
 	}
+}
 
-//}
